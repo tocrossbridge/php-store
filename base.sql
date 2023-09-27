53cb8a27be2d42f40b788dab7bfa001a973c9348
bea <beatriztattooer@gmail.com>
2023-09-27 19:37:06 -0300

    :package: DB

--- base.sql
+++ base.sql
@@ -1,71 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 16-Set-2023 às 03:09
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `base`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cadastro`
---
-
-CREATE TABLE `cadastro` (
-  `id_cad` int(10) NOT NULL,
-  `nome_cad` varchar(100) NOT NULL,
-  `email_cad` varchar(100) NOT NULL,
-  `data_cad` date NOT NULL,
-  `status_cad` int(1) NOT NULL
+CREATE TABLE `produtos` (
+  `id_prod` int(9) NOT NULL,
+  `nome_prod` varchar(30) NOT NULL,
+  `setor_prod` int(9) NOT NULL,
+  `custo_prod` float(10) NOT NULL,
+  `venda_prod` float(10) NOT NULL,
+  `estoque_prod` int(5) NOT NULL,
+  `situacao_prod` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `cadastro`
---
 
-INSERT INTO `cadastro` (`id_cad`, `nome_cad`, `email_cad`, `data_cad`, `status_cad`) VALUES
-(1, 'Rodrigo', 'prof@rodrigomocellin.com.br', '2023-09-15', 0),
-(2, 'a', 'a', '2023-09-15', 1),
-(3, 'Pedro', 'pedro@pedro.com', '2023-09-15', 0),
-(4, 'Maria', 'maria@maria.com.br', '1980-11-15', 1);
+CREATE TABLE `setores` (
+  `id_set` int(9) NOT NULL,
+  `nome_set` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `cadastro`
---
-ALTER TABLE `cadastro`
-  ADD PRIMARY KEY (`id_cad`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`setor_prod`);
 
---
--- AUTO_INCREMENT de tabela `cadastro`
---
-ALTER TABLE `cadastro`
-  MODIFY `id_cad` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `setores` (`id_set`, `nome_set`) VALUES (1, 'Legumes');
+
+INSERT INTO `produtos` (`id_prod`, `nome_prod`, `custo_prod`, `venda_prod`, `estoque_prod`, `situacao_prod`, `setor_prod`) VALUES
+(1, 'Batata', 2.0, 2.5, 2, 1, 1);
