ae57c427f7a9b0cece31579fa491f29fd460edcc
bea <beatriztattooer@gmail.com>
2023-09-28 20:27:02 -0300

    :construction: WIP

--- src/save.php
+++ src/save.php
@@ -3,4 +3,9 @@
 require_once __DIR__."\config.php";
 require_once SITE_ROOT."\config.php";
 
+try{
+    // TODO: eventualmente faço :D
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
 ?>
